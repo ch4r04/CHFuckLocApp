1afa4abf9684a3019a907478821b2dc5eb6cf923
hya <hya@hyadeimac.lan>
2016-08-29 16:18:54 +0800

    Initial Commit

---
+++ CHFuckLocApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHFuckLocApp
+//
+//  Created by hya on 16/8/29.
+//  Copyright (c) 2016年 ch4r0n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
